9ce3c01f80a1f187735f23b267763ec2b8d250ee
Rafael <khachikyan38@gmail.com>
2021-11-10 22:16:32 +0300

    final project

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
+const btn = document.querySelector(".sunnyside__btn-js");
 const menu = document.querySelector(".sunnyside__list-js");
-const button = document.querySelector(".sunnyside__hamburger-js");
+const hamburger = document.querySelector(".sunnyside__hamburger-js");
 
-button.addEventListener("click", () => {
+hamburger.addEventListener("click", () => {
   menu.classList.toggle("sunnyside__list-hidden");
+  menu.classList.contains("sunnyside__list-hidden")
+    ? btn.setAttribute("aria-expanded", "false")
+    : btn.setAttribute("aria-expanded", "true");
 });
